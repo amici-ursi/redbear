945995685175fb06de8ba71dd6d38affc202555e
strentax <strentax@gmail.com>
2020-08-14 15:56:43 -0400

    Fix: use self on values the class ought to hold on to
    Classes, amirite?

--- redbear.py
+++ redbear.py
@@ -20,7 +20,7 @@ class Redbear(commands.Cog):
 
         self.config.register_guild(**default_guild)
 
-        iam_role_list = {'she',
+        self.iam_role_list = {'she',
                  'they',
                  'he',
                  'United States',
@@ -58,74 +58,69 @@ class Redbear(commands.Cog):
                  'Brexiteers',
                  'No News'}
 
-        #client = discord.Client()
-        client = bot
-        #if client is None:
-        #    print("no client was found")
         load_errors = dict()
         try:
             #main guild
-            our_guild = bot.get_guild(441423477648523284)
-            if our_guild is None:
+            self.our_guild = bot.get_guild(441423477648523284)
+            if self.our_guild is None:
                 load_errors[0]="our_guild not set (and therefore, no roles were set)."
             #roles
             else:
-                our_roles = our_guild.roles
-                muted_role = discord.utils.get(our_roles, name='mute-1')
-                if muted_role is None:
+                our_roles = self.our_guild.roles
+                self.muted_role = discord.utils.get(our_roles, name='mute-1')
+                if self.muted_role is None:
                     load_errors[1] = "muted_role not set."
-                mute_2_role = discord.utils.get(our_roles, id=743162783679381737)
-                if mute_2_role is None:
+                self.mute_2_role = discord.utils.get(our_roles, id=743162783679381737)
+                if self.mute_2_role is None:
                     load_errors[2] = "mute_2_role not set."
-                moderator_role = discord.utils.get(our_roles, id=743162754596208790)
-                if moderator_role is None:
+                self.moderator_role = discord.utils.get(our_roles, id=743162754596208790)
+                if self.moderator_role is None:
                     load_errors[3] = "moderator_role not set."
-                embed_role = discord.utils.get(our_roles, name='embed')
-                if embed_role is None:
+                self.embed_role = discord.utils.get(our_roles, name='embed')
+                if self.embed_role is None:
                     load_errors[4] = "embed_role not set."
-                interviewee_role = discord.utils.get(our_roles, name="interviewee")
-                if interviewee_role is None:
+                self.interviewee_role = discord.utils.get(our_roles, name="interviewee")
+                if self.interviewee_role is None:
                     load_errors[5] = "interviewee_role not set."
-                beardy_role = discord.utils.get(our_roles, name='Beardy')
-                if beardy_role is None:
+                self.beardy_role = discord.utils.get(our_roles, name='Beardy')
+                if self.beardy_role is None:
                     load_errors[6] = "beardy_role not set."
-                modmute_role = discord.utils.get(our_roles, name='modmute')
-                if modmute_role is None:
+                self.modmute_role = discord.utils.get(our_roles, name='modmute')
+                if self.modmute_role is None:
                     load_errors[7] = "modmute_role not set."
     
             #channels
-            usernotes_channel = bot.get_channel(743161064308473926)
-            print(usernotes_channel.name)
-            if usernotes_channel is None:
+            self.usernotes_channel = bot.get_channel(743161064308473926)
+            if self.usernotes_channel is None:
                 load_errors[8] = "usernotes_channel not set."
-            timeout_channel = bot.get_channel(743161355976048660)
-            if timeout_channel is None:
+            self.timeout_channel = bot.get_channel(743161355976048660)
+            if self.timeout_channel is None:
                 load_errors[9] = "timeout_channel not set."
-            interview_channel = bot.get_channel(743162226201985256)
-            if interview_channel is None:
+            self.interview_channel = bot.get_channel(743162226201985256)
+            if self.interview_channel is None:
                 load_errors[10] = "interview_channel not set."
-            beardy_channel = bot.get_channel(743161445755256924)
-            if beardy_channel is None:
+            self.beardy_channel = bot.get_channel(743161445755256924)
+            if self.beardy_channel is None:
                 load_errors[11] = "beardy_channel not set."
-            help_commands_channel = bot.get_channel(743161401710739476)
-            if help_commands_channel is None:
+            self.help_commands_channel = bot.get_channel(743161401710739476)
+            if self.help_commands_channel is None:
                 load_errors[12] = "help_commands_channel not set."
-            curated_news_channel = bot.get_channel(743161545269313657)
-            if curated_news_channel is None:
+            self.curated_news_channel = bot.get_channel(743161545269313657)
+            if self.curated_news_channel is None:
                 load_errors[13] = "curated_news_channel not set."
-            tweets_channel = bot.get_channel(743161581982056509)
-            if tweets_channel is None:
+            self.tweets_channel = bot.get_channel(743161581982056509)
+            if self.tweets_channel is None:
                 load_errors[14] = "tweets_channel not set."
-            low_effort_channel  = bot.get_channel(743161613078495303)
-            if low_effort_channel is None:
+            self.low_effort_channel  = bot.get_channel(743161613078495303)
+            if self.low_effort_channel is None:
                 load_errors[15] = "low_effort_channel not set."
-            bot_spam_channel_id  = bot.get_channel(743161650718179469)
-            if bot_spam_channel_id is None:
+            self.bot_spam_channel_id  = bot.get_channel(743161650718179469)
+            if self.bot_spam_channel_id is None:
                 load_errors[16] = "bot_spam_channel_id not set."
             
             #users
-            amici = bot.get_user(234842700325715969)
-            if amici is None:
+            self.amici = bot.get_user(234842700325715969)
+            if self.amici is None:
                 load_errors[17] = "amici not set."
 
             if len(load_errors) > 0:
@@ -138,7 +133,7 @@ class Redbear(commands.Cog):
 
     @commands.command()
     async def mute(self, ctx):
-        await ctx.send("test")
+        await ctx.send(self.tweets_channel.name)
 
     @commands.Cog.listener()
     async def on_message(self, message):
